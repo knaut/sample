58d3274c3e2d9ebed735b21d0c2eeea78fef14a7
knaut <daniel@smallinvisiblemachines.com>
2020-07-26 19:22:20 -0700

    basic components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+
+  Card,
+  CardBody,
+  CardTitle,
+  Table
+} from 'reactstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function AppNav() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Navbar color='light' light>
+      <NavbarBrand>Toro</NavbarBrand>
+      <Nav navbar>
+        <NavItem>
+          <NavLink href='#'>Test</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  )
 }
 
-export default App;
+
+
+class SchemaTable extends Component {
+  render() {
+    const { columns, title } = this.props
+
+    const columnHeaderCells = []
+    for (let i = 0; columns.length > i; ++i) {
+      const column = columns[i]
+
+      columnHeaderCells.push(
+        <th>{column.title}</th>
+      )
+    }
+
+    return (
+      <Card>
+        <CardBody>
+          <CardTitle>{title}</CardTitle>
+          <Table>
+            <thead>
+              {columnHeaderCells}
+            </thead>
+          </Table>
+        </CardBody>
+      </Card>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    isFetched: false,
+    tables: []
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('https://interview.torodata.io/tables')
+      const json = await response.json()
+
+      console.log(json)
+
+      this.setState({
+        tables: json
+      })
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render() {
+    const { tables, isFetched } = this.state
+    const tablesCards = []
+
+    for (let i = 0; tables.length > i; ++i) {
+      const table = tables[i]
+
+      tablesCards.push(
+        <SchemaTable
+          columns={table.columns}
+          title={table.title}
+        />
+      )
+    }
+
+    return (
+      <>
+        <AppNav/>
+        {isFetched ? <SchemaTable/> : null}
+      </>
+    )
+  }
+}
+
+export default App
